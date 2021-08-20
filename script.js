048c9e63e767bf2879fdc07a170db8cd8cdfc0a0
Kyle McDowell <k.mcdowellr@gmail.com>
2021-08-19 18:03:58 -0700

    while loop

--- script.js
+++ script.js
@@ -14,14 +14,30 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function writePassword() {
+  // When button is clicked, prompt user to enter password length (between 8 and 128)
+  var pwdLength = window.prompt('How many characters would you like your password to be? Enter a length between 8 and 128.')
+
+  // Confirm user input.
+  while (pwdLength < 8 || pwdLength > 128) {
+
+    var pwdLength = window.prompt('How many characters would you like your password to be? Enter a length between 8 and 128.')
+
+  }
+  var confirmLength = window.confirm('You entered ' + pwdLength + '. Hit OK to confirm, or cancel to try again.')
+    if (confirmLength) { 
+    // After confirmation, prompt user to enter character types: Lowercase, uppercase, numberic, special characters.
+      var charLowerCase = confirm('Would you like to use lowercase letters? OK for yes, Cancel for no.')
+      if (charLowerCase) {
+       
+      }
+    } 
+    
+});
 
 
-// When button is clicked, prompt user to enter password length (between 8 and 128)
 
-// After password length is chosen, validate the chosen length. Return to prompt if incorrect.
 
-// After confirmation, prompt user to enter character types: Lowercase, uppercase, numberic, special characters.
 
 // After character types are chosen, validate character types with user. Return to prompt if incorrect.
 
